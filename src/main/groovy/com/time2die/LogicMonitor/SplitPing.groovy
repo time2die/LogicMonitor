75eb9a3684163fd94bba04ca13b1bc034647cda9
time2die <time2rfc@gmail.com>
2016-04-18 00:26:38 +0300

    ping and get result

---
+++ src/main/groovy/com/time2die/LogicMonitor/SplitPing.groovy
@@ -0,0 +1,58 @@
+def text = """
+Pinging 10.9.8.1 with 32 bytes of data:
+
+Reply from 10.9.8.1: bytes=32 time<1ms TTL=64
+
+Reply from 10.9.8.1: bytes=32 time=4ms TTL=64
+
+Reply from 10.9.8.1: bytes=32 time=1ms TTL=64
+
+
+
+Ping statistics for 10.9.8.1:
+
+    Packets: Sent = 3, Received = 3, Lost = 0 (0% loss),
+
+Approximate round trip times in milli-seconds:
+
+    Minimum = 0ms, Maximum = 4ms, Average = 1ms
+"""
+
+text = """Pinging 192.168.1.1 with 32 bytes of data:
+
+Request timed out.
+
+Request timed out.
+
+Request timed out.
+
+
+
+Ping statistics for 192.168.1.1:
+
+    Packets: Sent = 3, Received = 0, Lost = 3 (100% loss),
+
+
+"""
+
+String lostString
+String aproximate
+text.trim().split("\n").each {
+    if(it.length() > 1) {
+        if (it.contains("loss"))
+            lostString = it.trim()
+        if(it.contains("Average"))
+            aproximate = it.trim()
+    }
+}
+
+println lostString
+println aproximate
+
+String lostMS = new StringBuffer(lostString.toString()).substring(lostString.indexOf('(')+1,lostString.indexOf(')')).split("loss")[0].split("%")[0]
+println lostMS
+
+if(aproximate != null) {
+    String average = aproximate.substring(aproximate.lastIndexOf('=') + 1, aproximate.length()).split("ms")[0].trim()
+    println average
+}
--- src/main/groovy/com/time2die/LogicMonitor/test.groovy
+++ src/main/groovy/com/time2die/LogicMonitor/test.groovy
@@ -3,48 +3,83 @@ import com.santaba.agent.groovyapi.jmx.*
 import groovy.json.JsonSlurper
 import org.xbill.DNS.*
 
-
-def minutes_of_SDT = 60
-
+//def minutes_of_SDT = 60
 def my_displayname = "EdgeRouter"
-
 def company = "suding"
 def api_user = "api4"
 def api_pass = "api.805"
 
 def qty_of_pings_per_batch = 3
 def qty_of_batches = 2
-def seconds_between_batches = 9
+def seconds_between_batches = 1
+
 def threshold_average_ms = 77
 def threshold_ping_loss_percent = 70
 
 
 def displayname = URLEncoder.encode(my_displayname, "UTF-8")
-
 String hostInfo = HTTP.body("https://" + company + ".logicmonitor.com/santaba/rpc/getHost?c=$company&u=$api_user&p=$api_pass&displayName=$displayname")
-
 
 def hostInfoJSON = new JsonSlurper().parseText(hostInfo)
 def id = hostInfoJSON.data.id
 String ips = hostInfoJSON.data.properties.get "system.ips"
 
 def host = ips.split(",")[0]
+def pingCommand = """ping  -n $qty_of_pings_per_batch $host"""
+// pingCommand = """ping  -n $qty_of_pings_per_batch 192.168.1.1"""
 
 
-println "hostname: $host"
-def mycommand = """ping  -n $qty_of_pings_per_batch $host"""
-def proc = mycommand.execute()
-proc.waitFor()
-println "return code: ${proc.exitValue()}"
+//    println "hostname: $host"
+int summLoss = 0
+int summAve = 0
 
-def buffer_contents = proc.in.text
-println buffer_contents
-def average = buffer_contents.split("Average = ")
+//cycles for ping
+for (int i = 0; i < qty_of_batches; i++) {
+    def proc = pingCommand.execute()
+    proc.waitFor()
+//    println "return code: ${proc.exitValue()}"
 
-if (average.size() > 1) {
-    def msAverage = average[1]
-    println "ms: $msAverage"
+    def buffer_contents = proc.in.text
+
+    String lostString
+    String aproximate
+    buffer_contents.trim().split("\n").each {
+        if (it.length() > 1) {
+            if (it.contains("loss"))
+                lostString = it.trim()
+            if (it.contains("Average"))
+                aproximate = it.trim()
+        }
+    }
+
+//println lostString
+//println aproximate
+
+    String lostMS = new StringBuffer(lostString.toString()).substring(lostString.indexOf('(') + 1, lostString.indexOf(')')).split("loss")[0].split("%")[0]
+//    println "lostMS:$lostMS"
+
+    summLoss = summLoss + Integer.valueOf(lostMS)
+
+    String averagef
+    if (aproximate != null) {
+        averagef = aproximate.substring(aproximate.lastIndexOf('=') + 1, aproximate.length()).split("ms")[0].trim()
+//        println "average:$averagef"
+        summAve = summAve + Integer.valueOf(averagef)
+    }
+
+
+    sleep(seconds_between_batches * 1000l)
 }
 
+summAve = summAve / qty_of_batches
+summLoss = summLoss / qty_of_batches
 
+//println "sumAve:$summAve"
+//println "sumLose:$summLoss"
 
+if(summLoss < threshold_ping_loss_percent && summAve < threshold_average_ms){
+    println "workFine: $summAve $summLoss"
+    return 0 ;
+}
+
+println "i am here"
